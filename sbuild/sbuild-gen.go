8c28d817261550bda29467d8a1171d68db5756c0
kimtinh <onlyesterday.16@gmail.com>
2019-02-14 17:42:18 +0700

    Add a summary page for debian-cross-patches
    Signed-off-by: kimtinh <onlyesterday.16@gmail.com>

--- sbuild/sbuild-gen.go
+++ sbuild/sbuild-gen.go
@@ -31,18 +31,89 @@ import (
 const (
 	sbuildLog            = "https://raw.githubusercontent.com/dothanhtrung/build-log/master/"
 	debian_cross_patches = "https://github.com/meta-debian/debian-cross-patches/tree/master/"
+	white                = "#ecf0f1"
+	green                = "#2ecc71"
+	orange               = "#e67e22"
 )
 
-func main() {
+func genDCB(notes map[string]interface{}) {
+	pkgListFile, _ := ioutil.ReadFile("debian-cross-patches-list.txt")
+	lines := strings.Split(string(pkgListFile), "\n")
+
+	html := ""
+	html += "<html><head><title>debian-cross-patches</title>\n"
+	html += "<script src=\"https://kryogenix.org/code/browser/sorttable/sorttable.js\"></script>\n"
+	html += "<script src=\"../js/default.js\"></script>\n"
+	html += "<link rel=\"stylesheet\" type=\"text/css\" id=\"table_row_counter\" href=\"../css/table_row_counter.css\"/>\n"
+	html += "</head><body>\n"
+	html += "<h1>debian-cross-patches summary</h1>\n"
+	html += "<h3><a href=\"https://github.com/meta-debian/debian-cross-patches\">Repository</a></h3>"
+	html += "Packages, which have bug report but have not been submitted, mean they have been fixed by others."
+	html += "<br/>"
+	html += "<br/><table class=\"sortable\" id=\"sortable\">\n"
+	html += "<tr bgcolor=\"#bdc3c7\">" +
+		"<th class=\"sorttable_nosort\"></th>" +
+		"<th>Source Name</th>" +
+		"<th>Submitted</th>" +
+		"<th>Debian Bug</th>" +
+		"<th>Remark</th></tr>\n"
+
+	for i := 0; i < len(lines)-1; i++ {
+		pkg := lines[i]
+
+		remark := ""
+		bugs := ""
+		submitted := ""
+		bgcolor := white
+		if notes[pkg] != nil {
+			data := notes[pkg].(map[string]interface{})
+			if data["remark"] != nil {
+				remark = data["remark"].(string)
+			}
+
+			if data["bugs"] != nil {
+				bugsArr := data["bugs"].([]interface{})
+				if len(bugsArr) > 0 {
+					bgcolor = orange
+				}
+				for j := 0; j < len(bugsArr); j++ {
+					bugs += "<a href=\"https://bugs.debian.org/" + bugsArr[j].(string) +
+						"\">#" + bugsArr[j].(string) + "</a>"
+					if j != len(bugsArr)-1 {
+						bugs += "<br/>"
+					}
+				}
+			}
+
+			if data["submitted"] != nil {
+				if data["submitted"].(bool) {
+					submitted = "Yes"
+					bgcolor = green
+				}
+			}
+		}
+
+		html += "<tr bgcolor=\"" + bgcolor + "\"><td></td>\n" +
+			"<td>" + pkg + "</td>\n" +
+			"<td>" + submitted + "</td>\n" +
+			"<td>" + bugs + "</td>\n" +
+			"<td>" + remark + "</td>\n" +
+			"</tr>"
+	}
+
+	html += "</table></body></html>"
+
+	f, _ := os.Create("debian-cross-patches.html")
+	defer f.Close()
+	f.WriteString(html)
+	f.Sync()
+	w := bufio.NewWriter(f)
+	w.Flush()
+}
+
+func genIndex(notes map[string]interface{}) {
 	statusFile, _ := ioutil.ReadFile("sbuild-result")
 	lines := strings.Split(string(statusFile), "\n")
-
-	var remarks map[string]interface{}
-	remarkFile, _ := ioutil.ReadFile("remark.json")
-	json.Unmarshal(remarkFile, &remarks)
-
-	f, _ := os.Create("index.html")
-	defer f.Close()
 
 	html := "<html><head><title>Sbuild Status</title>\n"
 	html += "<script src=\"https://kryogenix.org/code/browser/sorttable/sorttable.js\"></script>\n"
@@ -50,6 +121,7 @@ func main() {
 	html += "<link rel=\"stylesheet\" type=\"text/css\" id=\"table_row_counter\" href=\"../css/table_row_counter.css\"/>\n"
 	html += "</head><body>\n"
 	html += "<h1>Debian cross-build state</h1>\n"
+	html += "<h3><a href=\"./debian-cross-patches.html\">Summary of debian-cross-patches</a></h3>\n"
 	html += "Build Architecture: amd64<br/>Host Architecture: armhf<br/>##Summary##<br/>\n"
 	html += "<br/><input type=\"checkbox\"/ onclick=\"tableRemoveCounter(this);\"> Disable row counter. This helps table sort faster. (Re-enabling will take time)\n"
 	html += "<br/><table class=\"sortable\" id=\"sortable\">\n"
@@ -59,6 +131,7 @@ func main() {
 		"<th class=\"sorttable_nosort\">Version</th>" +
 		"<th>Status</th>" +
 		"<th>Build At</th>" +
+		"<th>Debian Bug</th>" +
 		"<th>Remark</th></tr>\n"
 
 	successCount := 0
@@ -76,21 +149,35 @@ func main() {
 		if status == "attempted" {
 			bgcolor = "#e74c3c"
 		} else if status == "skipped" {
-			bgcolor = "#ecf0f1"
+			bgcolor = white
 		} else if status == "successful" {
-			bgcolor = "#2ecc71"
+			bgcolor = green
 			successCount++
 		} else if status == "given-back" {
-			bgcolor = "#e67e22"
+			bgcolor = orange
 		}
 
 		remark := ""
-		if remarks[name] != nil {
-			remark = remarks[name].(string)
+		bugs := ""
+		if notes[name] != nil {
+			data := notes[name].(map[string]interface{})
+			if data["remark"] != nil {
+				remark = data["remark"].(string)
+			}
+			if data["bugs"] != nil {
+				bugsArr := data["bugs"].([]interface{})
+				for j := 0; j < len(bugsArr); j++ {
+					bugs += "<a href=\"https://bugs.debian.org/" + bugsArr[j].(string) +
+						"\">#" + bugsArr[j].(string) + "</a>"
+					if j != len(bugsArr)-1 {
+						bugs += "<br/>"
+					}
+				}
+			}
 		}
 		if _, err := os.Stat("./debian-cross-patches/" + name); !os.IsNotExist(err) {
 			if remark != "" {
-				remark += "</br>"
+				remark += "<br/>"
 			}
 			remark += "\n (<a href=\"" + debian_cross_patches + name + "\">debian-cross-patches</a>)"
 		}
@@ -101,6 +188,7 @@ func main() {
 			"<td>" + version + "</td>\n" +
 			"<td>" + status + "</td>\n" +
 			"<td sorttable_customkey=\"" + customTimestmp + "\">" + timestmp + "</td>\n" +
+			"<td>" + bugs + "</td>\n" +
 			"<td>" + remark + "</td></tr>\n"
 		html += row
 	}
@@ -108,8 +196,19 @@ func main() {
 	html = strings.Replace(html, "##Summary##", "Success: "+strconv.Itoa(successCount)+"<br/>Total: "+strconv.Itoa(len(lines)-1), -1)
 	html += "</table></body></html>"
 
+	f, _ := os.Create("index.html")
+	defer f.Close()
 	f.WriteString(html)
 	f.Sync()
 	w := bufio.NewWriter(f)
 	w.Flush()
+}
+
+func main() {
+	var notes map[string]interface{}
+	notesFile, _ := ioutil.ReadFile("notes.json")
+	json.Unmarshal(notesFile, &notes)
+
+	genIndex(notes)
+	genDCB(notes)
 }
