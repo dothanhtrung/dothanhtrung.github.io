e67833e0166c9e65a583aa9f1f8772b2742b6b3a
Trung Do <trung.dothanh@toshiba-tsdv.com>
2019-06-24 10:37:15 +0700

    Move sbuild status to https://tswcos.github.io/sbuild/
    Signed-off-by: Trung Do <trung.dothanh@toshiba-tsdv.com>

--- js/default.js
+++
@@ -1,7 +0,0 @@
-function tableRemoveCounter(e) {
-    if (e.checked) {
-        document.getElementById("table_row_counter").disabled = true;
-    } else {
-        document.getElementById("table_row_counter").disabled = false;
-    }
-}
